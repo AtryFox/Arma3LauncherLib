192f1248c643a18586293326546747fc3f2396b0
Benjamin Leuckefeld <code@deratrox.de>
2016-05-28 23:56:31 +0200

    added interface

---
+++ Arma3LauncherLib/Model/IArmaServer.cs
@@ -0,0 +1,10 @@
+﻿using DerAtrox.Arma3LauncherLib.SSQLib.Model;
+
+namespace DerAtrox.Arma3LauncherLib.Model {
+    public interface IArmaServer {
+        ServerInfo GetServerInfo();
+        PlayerInfo[] GetPlayerList();
+        int PingServer();
+        void Connect();
+    }
+}
