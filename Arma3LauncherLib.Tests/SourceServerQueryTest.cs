ce6d4e70c5434d3fef68b6e397a650cfcc83c2b1
Benjamin Leuckefeld <code@deratrox.de>
2016-05-29 01:24:42 +0200

    fixed UnitTests

--- Arma3LauncherLib.Tests/SourceServerQueryTest.cs
+++ Arma3LauncherLib.Tests/SourceServerQueryTest.cs
@@ -8,37 +8,37 @@ namespace DerAtrox.Arma3LauncherLib.Tests {
     public class SourceServerQueryTest {
         [Test]
         public void TestFakePlayerServerThrowsExceptionWithIpEndpoint() {
-            Assert.ThrowsAsync(typeof(SourceServerException), async () => {
+            Assert.Throws(typeof(SourceServerException), () => {
                 SourceServerQuery query = new SourceServerQuery();
                 //127.0.0.2 set to stop loopback potentially resolving and failing test
-                await query.Players(new IPEndPoint(IPAddress.Parse("127.0.0.2"), 2303));
+                query.Players(new IPEndPoint(IPAddress.Parse("127.0.0.2"), 2303));
             });
         }
 
         [Test]
         public void TestFakePlayerServerThrowsExceptionWithStringIp() {
-            Assert.ThrowsAsync(typeof(SourceServerException), async () => {
+            Assert.Throws(typeof(SourceServerException), () => {
                 SourceServerQuery query = new SourceServerQuery();
                 //127.0.0.2 set to stop loopback potentially resolving and failing test
-                await query.Players("127.0.0.2", 2303);
+                query.Players("127.0.0.2", 2303);
             });
         }
 
         [Test]
         public void TestFakeServerThrowsExceptionWithIpEndpoint() {
-            Assert.ThrowsAsync(typeof(SourceServerException), async () => {
+            Assert.Throws(typeof(SourceServerException), () => {
                 SourceServerQuery query = new SourceServerQuery();
                 //127.0.0.2 set to stop loopback potentially resolving and failing test
-                await query.Server(new IPEndPoint(IPAddress.Parse("127.0.0.2"), 2303));
+                query.Server(new IPEndPoint(IPAddress.Parse("127.0.0.2"), 2303));
             });
         }
 
         [Test]
         public void TestFakeServerThrowsExceptionWithStringIp() {
-            Assert.ThrowsAsync(typeof(SourceServerException), async () => {
+            Assert.Throws(typeof(SourceServerException), () => {
                 SourceServerQuery query = new SourceServerQuery();
                 //127.0.0.2 set to stop loopback potentially resolving and failing test
-                await query.Server("127.0.0.2", 2303);
+                query.Server("127.0.0.2", 2303);
             });
         }
 
