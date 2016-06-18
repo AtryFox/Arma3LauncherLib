da9fd668b067916930f694c7f13f2387b1f394c4
Benjamin Leuckefeld <code@deratrox.de>
2016-06-18 12:55:08 +0200

    removed deprecated test

--- Arma3LauncherLib.Tests/UtilsTest.cs
+++ Arma3LauncherLib.Tests/UtilsTest.cs
@@ -4,12 +4,5 @@ using NUnit.Framework;
 namespace DerAtrox.Arma3LauncherLib.Tests {
     [TestFixture]
     public class UtilsTest {
-
-        [Test]
-        public void TestSerializeProfilename() {
-            string serialized = ProfileUtils.SerializeProfilename("qweÖÜÄöüä");
-
-            Assert.AreEqual(serialized, "qweÃÃÃÃ¶Ã¼Ã¤");
-        }
     }
 }
